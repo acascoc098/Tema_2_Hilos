ad1dd9a4f0235acba896058c1e3aec00ae110372
acascoc098 <acascoc098@g.educaand.es>
2023-11-21 11:14:07 +0100

    Creación del ejercicio 4, mirar el problema de que a veces lo hace más de una vez.

---
+++ Ejercicio 4/src/NumeroOculto.java
@@ -0,0 +1,24 @@
+public class NumeroOculto {
+    private int numAdivinar = (int) (Math.random()*10+1);
+    private boolean condicion = false;
+
+    public NumeroOculto() {
+    }
+
+    public int getNumAdivinar() {
+        return numAdivinar;
+    }
+
+    public boolean isCondicion() {
+        return this.condicion;
+    }
+
+    synchronized public int verSi(int numAMirar){
+        int num = 0;
+        if (this.numAdivinar == numAMirar){
+            condicion = true;
+            num = 1;
+        }
+        return num;
+    }
+}
