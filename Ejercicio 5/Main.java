6826ee129ba0cecbbdad0e0fe09ab3e31459c24c
acascoc098 <acascoc098@g.educaand.es>
2023-11-21 15:00:46 +0100

    Implementación en la clase main.

--- Ejercicio 5/Main.java
+++ Ejercicio 5/Main.java
@@ -4,10 +4,18 @@ public class Main {
         Cuenta c1 =  new Cuenta(20500,"ES388388433883838388484");
         Cuenta c2 = new Cuenta(50000,"ES99090499388484889399");
 
-        Hilo h1 = new Hilo("hilo 1",c1,c2);
-        Hilo h2 = new Hilo("hilo 2",c1,c2);
+        Hilo h1 = new Hilo("Castilla",c1,c2);
+        Hilo h2 = new Hilo("Castilla Andrea",c1,c2);
 
-        
+        h1.start();
+        h2.start();
+
+        try {
+            h1.join();
+            h2.join();
+        } catch (InterruptedException e) {
+            System.out.println(e.getMessage());
+        }
 
     }
 }
