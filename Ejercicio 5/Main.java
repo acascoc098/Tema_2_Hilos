01ffe15a9ddf3b9ba5070561e80ba55e282d807c
acascoc098 <acascoc098@g.educaand.es>
2023-11-21 15:13:50 +0100

    Detalles.

--- Ejercicio 5/Main.java
+++ Ejercicio 5/Main.java
@@ -1,11 +1,13 @@
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("@author: acascoc098");
+
         Cuenta c1 =  new Cuenta(20500,"ES388388433883838388484");
         Cuenta c2 = new Cuenta(50000,"ES99090499388484889399");
 
         Hilo h1 = new Hilo("Castilla",c1,c2);
-        Hilo h2 = new Hilo("Castilla Andrea",c1,c2);
+        Hilo h2 = new Hilo("Castilla Andrea",c2,c1);
 
         h1.start();
         h2.start();
@@ -16,6 +18,9 @@ public class Main {
         } catch (InterruptedException e) {
             System.out.println(e.getMessage());
         }
+
+        System.out.println();
+        System.out.println("Andrea Castilla Cocera");
 
     }
 }
