98842cd06bc14edecdb2bf09f1131a5004fa9535
acascoc098 <acascoc098@g.educaand.es>
2023-11-27 20:58:14 +0100

    Documentación clase Puente.

--- Ejercicio 6/Puente.java
+++ Ejercicio 6/Puente.java
@@ -4,10 +4,19 @@ class Puente {
     private int cochesEnPuente = 0;
     private int pesoEnPuente = 0;
 
+    /**
+     * Método para saber si puede pasar el coche
+     * @param peso El peso del coche para saber si puede pasar
+     * @return boolean
+     */
     public synchronized boolean sePermitePaso(int peso) {
         return cochesEnPuente < MAX_COCHES && (pesoEnPuente + peso) <= PESO_MAXIMO;
     }
 
+    /**
+     * Método para modificar el estado del puente una vez haya salido un coche
+     * @param peso EL peso del coche
+     */
     public synchronized void finalizarPaso(int peso) {
         cochesEnPuente--;
         pesoEnPuente -= peso;
@@ -15,6 +24,11 @@ class Puente {
         notifyAll();
     }
 
+    /**
+     * Método que empieza el paso del coche si este tienen el paso permitido
+     * @param peso Peso del coche
+     * @throws InterruptedException
+     */
     public synchronized void iniciarPaso(int peso) throws InterruptedException {
         while (!sePermitePaso(peso)) {
             wait();
